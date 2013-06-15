89cdf89d5d4c647f5cfc8569b5ea36d10e68b5eb
James <james@PR2Lab-2.(none)>
2013-06-14 20:44:40 -0400

    Interim Commit.  Added timing sighandler to controller to see if can get it to run at the frequency it is expected to.

--- TAS.h
+++ TAS.h
@@ -101,6 +101,211 @@ typedef double Real;
 #define DEFAULT_INITDELAY_RTTIMER_SEC	0
 
 //-----------------------------------------------------------------------------
+// Timing Functions
+//-----------------------------------------------------------------------------
+
+// ------- Notes -------
+// -- Rusage --
+// rusage is unsatisfactory because it cannot measure the amount of time that the
+// child thread runs for.  It can only record this time if the child is joined or it can record
+// the time that the current process (coordinator) runs for.  The problem is that it cannot
+// effectively query the process time of an active child process/thread.  It cannot therefore monitor
+// the time that a controller runs for.
+
+// -- proc --
+// The granularity of proc is too small.  It can only measure at a scale determined by _SC_CLK_TCK
+// which is apparantly a default of 100 on current linux and osx based distributions meaning that the
+// smallest granularity available of clock would be in the centi-seconds domain where it is normal for
+// controllers to operate in the milliseconds domain.  As the quantum approaches 1 centisecond, the accuracy
+// gets wild.  Sometimes, the controller is indicated to run for 0 units, 1 unit, or 2 units.
+
+// -- gettimeofday vs RDTSC --
+// We are looking into RDTSC, but realistically, what is the difference between RDTSC and gettimeofday.
+// getttimeofday is at least microsecond granularity.  If RDTSC cannot differentiate between system time and
+// user time and between particular thread time, then it is really no different than gettimeofday.
+
+// -- RDTSC --
+// major potential issues.
+// Skew between cores
+// Frequency changes with clock
+
+// high level of granularity, closer to per cycle rather than per tick
+//
+
+// -- clock_gettime --
+// 10 us.  Per tick granularity
+
+//-----------------------------------------------------------------------------
+
+// inline assembly for rdtsc timer
+#define rdtscll(val) __asm__ __volatile__("rdtsc" : "=A" (val))
+
+//-----------------------------------------------------------------------------
+
+// reference 'man proc 5'
+struct proc_stat {
+    int pid;                        // %d
+    char comm[256];                 // %s
+    char state;                     // %c
+    int ppid;                       // %d
+    int pgrp;                       // %d
+    int session;                    // %d
+    int tty_nr;                     // %d
+    int tpgid;                      // %d
+    unsigned flags;                 // %u (%lu before kernel 2.6.22)
+    unsigned long minflt;           // %lu
+    unsigned long cminflt;          // %lu
+    unsigned long majflt;           // %lu
+    unsigned long cmajflt;          // %lu
+    unsigned long utime;            // %lu
+    unsigned long stime;            // %lu
+    long cutime;                    // %ld
+    long cstime;                    // %ld
+    long priority;                  // %ld
+    long nice;                      // %ld
+    long num_threads;               // %ld
+    long itrealvalue;               // %ld
+    unsigned long long starttime;   // %llu (%lu before kernel 2.6)
+    unsigned long vsize;            // %lu
+    long rss;                       // %ld
+    unsigned long rsslim;           // %lu
+    unsigned long startcode;        // %lu
+    unsigned long endcode;          // %lu
+    unsigned long startstack;       // %lu
+    unsigned long kstkesp;          // %lu
+    unsigned long kstkeip;          // %lu
+    unsigned long signal;           // %lu
+    unsigned long blocked;          // %lu
+    unsigned long sigignore;        // %lu
+    unsigned long sigcatch;         // %lu
+    unsigned long wchan;            // %lu
+    unsigned long nswap;            // %lu
+    unsigned long cnswap;           // %lu
+    int exit_signal;                // %d (since kernel 2.1.22)
+    int processor;          		// %d (since kernel 2.2.8)
+    unsigned rt_priority;           // %u (since kernel 2.5.19) (%lu before kernel 2.6.22)
+    unsigned policy;                // %u (since kernel 2.5.19) (%lu before kernel 2.6.22)
+    unsigned long long delayacct_blkio_ticks;	// %llu (since kernel 2.6.18)
+    unsigned long guest_time;       // %lu (since kernel 2.6.24)
+    unsigned int cguest_time;       // %ld (since kernel 2.6.24)
+};
+
+//-----------------------------------------------------------------------------
+
+static int read_proc_stat( std::string fname, struct proc_stat *s ) {
+
+    const char *format = "%d %s %c %d %d %d %d %d %u %lu %lu %lu %lu %lu %lu %ld %ld %ld %ld %ld %ld %llu %lu %ld %lu %lu %lu %lu %lu %lu %lu %lu %lu %lu %lu %lu %lu %d %d %u %u %llu %lu %ld";
+
+    FILE *proc;
+    proc = fopen( fname.c_str(),"r" );
+    if( proc ) {
+        if( 44 == fscanf( proc, format,
+            &s->pid,
+            s->comm,
+            &s->state,
+            &s->ppid,
+            &s->pgrp,
+            &s->session,
+            &s->tty_nr,
+            &s->tpgid,
+            &s->flags,
+            &s->minflt,
+            &s->cminflt,
+            &s->majflt,
+            &s->cmajflt,
+            &s->utime,
+            &s->stime,
+            &s->cutime,
+            &s->cstime,
+            &s->priority,
+            &s->nice,
+            &s->num_threads,
+            &s->itrealvalue,
+            &s->starttime,
+            &s->vsize,
+            &s->rss,
+            &s->rsslim,
+            &s->startcode,
+            &s->endcode,
+            &s->startstack,
+            &s->kstkesp,
+            &s->kstkeip,
+            &s->signal,
+            &s->blocked,
+            &s->sigignore,
+            &s->sigcatch,
+            &s->wchan,
+            &s->nswap,
+            &s->cnswap,
+            &s->exit_signal,
+            &s->processor,
+            &s->rt_priority,
+            &s->policy,
+            &s->delayacct_blkio_ticks,
+            &s->guest_time,
+            &s->cguest_time
+        )) {
+            fclose( proc );
+            return 1;
+        } else {
+            fclose( proc );
+            return 0;
+        }
+    } else {
+        return 0;
+    }
+}
+
+//-----------------------------------------------------------------------------
+
+static void print_proc_stat( struct proc_stat *stuff ) {
+    printf( "pid = %d\n", stuff->pid );
+    printf( "comm = %s\n", stuff->comm );
+    printf( "state = %c\n", stuff->state );
+    printf( "ppid = %d\n", stuff->ppid );
+    printf( "pgrp = %d\n", stuff->pgrp );
+    printf( "session = %d\n", stuff->session );
+    printf( "tty_nr = %d\n", stuff->tty_nr );
+    printf( "tpgid = %d\n", stuff->tpgid );
+    printf( "flags = %u\n", stuff->flags );
+    printf( "minflt = %lu\n", stuff->minflt );
+    printf( "cminflt = %lu\n", stuff->cminflt );
+    printf( "majflt = %lu\n", stuff->majflt );
+    printf( "cmajflt = %lu\n", stuff->cmajflt );
+    printf( "utime = %lu\n", stuff->utime );
+    printf( "stime = %lu\n", stuff->stime );
+    printf( "cutime = %ld\n", stuff->cutime );
+    printf( "cstime = %ld\n", stuff->cstime );
+    printf( "priority = %ld\n", stuff->priority );
+    printf( "nice = %ld\n", stuff->nice );
+    printf( "num_threads = %ld\n", stuff->num_threads );
+    printf( "itrealvalue = %ld\n", stuff->itrealvalue );
+    printf( "starttime = %llu\n", stuff->starttime );
+    printf( "vsize = %lu\n", stuff->vsize );
+    printf( "rss = %ld\n", stuff->rss );
+    printf( "rsslim = %lu\n", stuff->rsslim );
+    printf( "startcode = %lu\n", stuff->startcode );
+    printf( "endcode = %lu\n", stuff->endcode );
+    printf( "startstack = %lu\n", stuff->startstack );
+    printf( "kstkesp = %lu\n", stuff->kstkesp );
+    printf( "kstkeip = %lu\n", stuff->kstkeip );
+    printf( "signal = %lu\n", stuff->signal );
+    printf( "blocked = %lu\n", stuff->blocked );
+    printf( "sigignore = %lu\n", stuff->sigignore );
+    printf( "sigcatch = %lu\n", stuff->sigcatch );
+    printf( "wchan = %lu\n", stuff->wchan );
+    printf( "nswap = %lu\n", stuff->nswap );
+    printf( "cnswap = %lu\n", stuff->cnswap );
+    printf( "exit_signal = %d\n", stuff->exit_signal );
+    printf( "processor = %d\n", stuff->processor );
+    printf( "rt_priority = %u\n", stuff->rt_priority );
+    printf( "policy = %u\n", stuff->policy );
+    printf( "delayacct_blkio_ticks = %llu\n", stuff->delayacct_blkio_ticks );
+    printf( "guest_time = %lu\n", stuff->guest_time );
+    printf( "cguest_time = %ld\n", stuff->cguest_time );
+}
+
+//-----------------------------------------------------------------------------
 
 #endif // _TAS_H_
 
--- controller.cpp
+++ controller.cpp
@@ -141,8 +141,11 @@ ActuatorMessage control( const ActuatorMessage& msg ) {
     Real measured_position = msg.state.position;
     Real measured_velocity = msg.state.velocity;
 
-    Real desired_position = position_trajectory( time, 0.5, 0.0, PI );
-    Real desired_velocity = velocity_trajectory( time, 0.5, 0.0, PI );
+    //Real desired_position = position_trajectory( time, 0.5, 0.0, PI );
+    //Real desired_velocity = velocity_trajectory( time, 0.5, 0.0, PI );
+
+    Real desired_position = position_trajectory( time, 10.0, 0.0, PI );
+    Real desired_velocity = velocity_trajectory( time, 10.0, 0.0, PI );
 
     reply.command.torque = Kp * ( desired_position - measured_position ) + Kv * ( desired_velocity - measured_velocity );
 
@@ -219,6 +222,25 @@ void request_initial_state_then_publish_initial_command( void ) {
 
 //-----------------------------------------------------------------------------
 
+int fd_timer_to_controller[2];
+
+void initialize_pipes( void ) {
+    int flags;
+
+    // NOTE: a read channel needs to be blocking to force  block waiting for event
+    // NOTE: a write channel should be non-blocking (won't ever fill up buffer anyway)
+
+    if( pipe( fd_timer_to_controller ) != 0 ) {
+        throw std::runtime_error( "Failed to open fd_timer_to_controller pipe." ) ;
+    }
+    flags = fcntl( fd_timer_to_controller[0], F_GETFL, 0 );
+    fcntl( fd_timer_to_controller[0], F_SETFL, flags );
+    flags = fcntl( fd_timer_to_controller[1], F_GETFL, 0 );
+    fcntl( fd_timer_to_controller[1], F_SETFL, flags | O_NONBLOCK );
+}
+
+//-----------------------------------------------------------------------------
+
 struct timeval get_process_time( void ) {
     struct rusage ru;
     struct timeval tv;
@@ -228,7 +250,6 @@ struct timeval get_process_time( void ) {
 
     gettimeofday( &tv, NULL );
 
-
     return tv;
 }
 
@@ -236,6 +257,88 @@ struct timeval get_process_time( void ) {
 
 Real timeval_to_real( const struct timeval& tv ) {
     return (Real) tv.tv_sec + (Real) tv.tv_usec / (Real) USECS_PER_SEC;
+}
+
+//-----------------------------------------------------------------------------
+
+
+// The signal identifier for the controller's real-time timer
+#define RTTIMER_SIGNAL                          SIGRTMIN + 5
+
+// timer variables
+clockid_t _clock;
+sigset_t rttimer_mask;
+
+// The signal action to reference controller's process signal handler
+struct sigaction rttimer_sigaction;
+
+#define ERR_TIMER_NOERROR                       0
+#define ERR_TIMER_FAILURE_SIGACTION             1
+#define ERR_TIMER_FAILURE_SIGPROCMASK           2
+#define ERR_TIMER_FAILURE_CREATE                3
+#define ERR_TIMER_FAILURE_SETTIME               4
+#define ERR_TIMER_FAILURE_GETCLOCKID            5
+
+void interval_sighandler( int signum, siginfo_t *si, void *data ) {
+    printf( "(controller) timer event\n" );
+    char buf = 0;
+    write( fd_timer_to_controller[1], &buf, 1 );
+}
+
+int block_signal_rttimer( void ) {
+    if( sigprocmask( SIG_SETMASK, &rttimer_mask, NULL ) == -1 )
+       return ERR_TIMER_FAILURE_SIGPROCMASK;
+    return ERR_TIMER_NOERROR;
+}
+
+int unblock_signal_rttimer( void ) {
+    if( sigprocmask( SIG_UNBLOCK, &rttimer_mask, NULL ) == -1 )
+       return ERR_TIMER_FAILURE_SIGPROCMASK;
+    return ERR_TIMER_NOERROR;
+}
+
+int create_rttimer_monitor( void ) {
+    timer_t timerid;
+    struct sigevent sevt;
+    struct itimerspec its;
+
+    // Establish handler for timer signal
+    rttimer_sigaction.sa_flags = SA_SIGINFO;
+    rttimer_sigaction.sa_sigaction = interval_sighandler;
+    sigemptyset( &rttimer_sigaction.sa_mask );
+    if( sigaction( RTTIMER_SIGNAL, &rttimer_sigaction, NULL ) == -1 )
+       return ERR_TIMER_FAILURE_SIGACTION;
+
+    // intialize the signal mask
+    sigemptyset( &rttimer_mask );
+    sigaddset( &rttimer_mask, RTTIMER_SIGNAL );
+
+    // Block timer signal temporarily
+    if( block_signal_rttimer() != ERR_TIMER_NOERROR )
+        return ERR_TIMER_FAILURE_SIGPROCMASK;
+
+    if( clock_getcpuclockid( getpid( ), &_clock ) != 0 )
+        return ERR_TIMER_FAILURE_GETCLOCKID;
+
+    // Create the timer
+    sevt.sigev_notify = SIGEV_SIGNAL;
+    sevt.sigev_signo = RTTIMER_SIGNAL;
+    sevt.sigev_value.sival_ptr = &timerid;
+    if( timer_create( _clock, &sevt, &timerid ) == -1 )
+       return ERR_TIMER_FAILURE_CREATE;
+
+    its.it_value.tv_sec = 0;
+    its.it_value.tv_nsec = 1;
+    its.it_interval.tv_sec = 0;
+    its.it_interval.tv_nsec = 1000000;
+
+    // Set up the timer
+    if( timer_settime( timerid, 0, &its, NULL ) == -1 )
+        return ERR_TIMER_FAILURE_SETTIME;
+
+    // NOTE: The timer is blocked at this point.  It must be unblocked to function.
+
+    return ERR_TIMER_NOERROR;
 }
 
 //-----------------------------------------------------------------------------
@@ -262,6 +365,11 @@ int main( int argc, char* argv[] ) {
     amsgbuffer = ActuatorMessageBuffer( ACTUATOR_MESSAGE_BUFFER_NAME, ACTUATOR_MESSAGE_BUFFER_MUTEX_NAME );
     amsgbuffer.open( );
 
+    // ----------
+    initialize_pipes( );
+    create_rttimer_monitor( );
+    // ----------
+
     // query and publish initial values
     request_initial_state_then_publish_initial_command( );
 
@@ -274,9 +382,59 @@ int main( int argc, char* argv[] ) {
     // compute the end of the initial interval
     timeradd( &tv_interval_start, &tv_interval, &tv_interval_end );
 
+    // ----------
+    char buf = 0;
+    unblock_signal_rttimer( );
+    // ----------
+
     // start the main process loop
     while( 1 ) {
+        if( read( fd_timer_to_controller[0], &buf, 1 ) == -1 ) {
+            perror( "read()" );
+        }
 
+        // poll time
+        // Note: double edged: poll may block on system call, but using signal
+        // will definitely block and adds complexity that may invalidate the
+        // coordinator measuring of the controller
+        tv_current = get_process_time( );
+
+        // compute the actual time to compute the control output
+        // Note: current is absolute from beginning of process
+        // but control needs to be computed in terms of the time
+        // after the controller was initialized
+        timersub( &tv_current, &tv_start, &tv_control );
+
+        // Compute the time
+        Real t = timeval_to_real( tv_control );
+        if( VERBOSE ) printf( "(controller) Requesting state at time: %f\n", t );
+
+        state = get_state( t );
+
+        command = control( state );
+        command.header.type = MSG_TYPE_COMMAND;
+
+        if( VERBOSE ) printf( "(controller) Publishing command for time: %f\n", t );
+        publish_command( command );
+
+        // recompute start of the interval
+        // Note: the interval is computed on the time it was supposed to end
+        // not on the current time.  It is highly unlikely that the current will
+        // be on time (at the end) and the error could accumulate as a result
+        // so using the expected value instead of actual.  Error in micorseconds
+        memcpy( &tv_interval_start, &tv_interval_end, sizeof(timeval) );
+
+        // recompute the end of the interval
+        timeradd( &tv_interval_start, &tv_interval, &tv_interval_end );
+
+        if( VERBOSE ) printf( "(controller) Current: %f\n", timeval_to_real( tv_current ) );
+        if( VERBOSE ) printf( "(controller) Interval: %f\n", timeval_to_real( tv_interval ) );
+        if( VERBOSE ) printf( "(controller) Next interval start: %f\n", timeval_to_real( tv_interval_start ) );
+        if( VERBOSE ) printf( "(controller) Next interval end: %f\n", timeval_to_real( tv_interval_end ) );
+
+        cycle++;
+
+/*
         // poll time
         // Note: double edged: poll may block on system call, but using signal
         // will definitely block and adds complexity that may invalidate the
@@ -325,6 +483,7 @@ int main( int argc, char* argv[] ) {
         if( VERBOSE ) printf( "(controller) Next interval end: %f\n", timeval_to_real( tv_interval_end ) );
 
         cycle++;
+*/
     }
 
     amsgbuffer.close( );  // TODO : figure out a way to force a clean up
