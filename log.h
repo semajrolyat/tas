d9b8198fd2dd0b70a5776f2dbb28b330a7d64df0
James <james@PR2Lab-2.(none)>
2013-06-21 02:49:39 -0400

    refactored log_c to use system calls and file descriptors instead of streams so that a single file can be shared among processes.

--- log.h
+++ log.h
@@ -11,14 +11,20 @@ log.h
 
 #include <assert.h>
 #include <string.h>
-#include <iostream>
-#include <fstream>
+//#include <iostream>
+//#include <fstream>
+
+// Linux using file descriptors
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <unistd.h>
 
 //-----------------------------------------------------------------------------
 
 enum log_err_e {
     LOG_ERROR_NONE = 0,
     LOG_ERROR_OPEN,
+    LOG_ERROR_CLOSE,
     LOG_ERROR_WRITE
 };
 
@@ -32,9 +38,26 @@ enum log_channel_e {
 
 //-----------------------------------------------------------------------------
 
+int __open( const char* path, int oflags, int mode ) {
+    return open( path, oflags, mode );
+}
+
+//-----------------------------------------------------------------------------
+
+ssize_t __write( int fd, const void* buf, size_t count ) {
+    return write( fd, buf, count );
+}
+
+int __close( int fd ) {
+    return close( fd );
+}
+
+//-----------------------------------------------------------------------------
+
 class log_c {
 private:
-    std::ostream* str;
+    //std::ostream* str;
+    int fd;
     bool opened;
     log_channel_e channel;
     //-------------------------------------------------------------------------
@@ -54,6 +77,16 @@ public:
     }
 
     //-------------------------------------------------------------------------
+    
+    log_c( log_channel_e channel, const int& fd ) {
+	//opened = false;
+	this->channel = channel;
+	this->fd = fd;
+
+	//could add some assertions
+    }
+
+    //-------------------------------------------------------------------------
 
     virtual ~log_c( void ) { }
 
@@ -62,9 +95,9 @@ public:
 	assert( channel == LOG_CHANNEL_STDOUT || channel == LOG_CHANNEL_STDERR );
 
 	if( channel == LOG_CHANNEL_STDOUT ) {
-	    str = (std::ostream*)&std::cout;
+	    fd = 1;
 	} else if (channel == LOG_CHANNEL_STDERR) {
-	    str = (std::ostream*)&std::cerr;
+	    fd = 2;
 	}
 
 	opened = true;
@@ -74,46 +107,49 @@ public:
     log_err_e open( std::string filename ) {
 	assert( channel == LOG_CHANNEL_FILE );
 
-	std::ofstream* file = new std::ofstream( );
-
-        file->exceptions( std::ofstream::failbit );
-	try {
-            file->open( filename.c_str( ), std::ofstream::out | std::ofstream::trunc );
-        } catch ( std::ofstream::failure ex ) {
-            return LOG_ERROR_OPEN;
-        }
+	if( (fd = __open( filename.c_str(), O_WRONLY | O_CREAT | O_TRUNC,
+	                  S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH )) == -1 ) {
+	    return LOG_ERROR_OPEN;
+	}
 	opened = true;
-	str = file;
+	
         return LOG_ERROR_NONE;
     }
 
     //-------------------------------------------------------------------------
 
-    void write( std::string s ) {
+    log_err_e write( std::string s ) {
 	assert( opened );
 
-	(*str) << s;
+	if( __write( fd, s.c_str( ), s.size( ) ) == -1 )
+	    return LOG_ERROR_WRITE;
+	return LOG_ERROR_NONE;
     }
 
     //-------------------------------------------------------------------------
 
-    void writeln( std::string s ) {
-	assert( opened );
+    log_err_e writeln( std::string s ) {
+	// Note: implicit assert( opened ) provided by write
 
-	(*str) << s << std::endl;
+	if( write( s ) == LOG_ERROR_WRITE )
+	    return LOG_ERROR_WRITE;
+
+        if( __write( fd, "\n", 1 ) == -1 )
+	    return LOG_ERROR_WRITE;
+
+	return LOG_ERROR_NONE;
     }
 
     //-------------------------------------------------------------------------
 
-    void close( void ) {
+     log_err_e close( void ) {
+	assert( opened );
+
 	opened = false;
 
-	if( channel == LOG_CHANNEL_FILE ) {
-	    std::ofstream* file = (std::ofstream*)str;
-            file->close( );
-	    delete file;
-	}
-	str = NULL;
+	if( __close( fd ) == -1 ) 
+	    return LOG_ERROR_CLOSE;
+	return LOG_ERROR_NONE;
     }
 };
 
